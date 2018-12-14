4ccb8666f060b689e1090ec23cf3a0f334160bf5
Sharma, Amit <asharma@agtinternational.com>
2018-12-14 01:13:51 +0100

    Adding Divider list in the barcode list page and introduce Barcode interface

---
+++ src/lib/src/interfaces/IBarcode.dart
@@ -0,0 +1,13 @@
+class Barcode {
+  String _code;
+  String get code => _code;
+  set code(String pCode) {
+    _code = pCode;
+  }
+
+  DateTime _timeStamp;
+  DateTime get timeStamp => _timeStamp;
+  set timeStamp(DateTime pTimeStamp) {
+    _timeStamp = pTimeStamp;
+  }
+}
--- src/lib/src/pages/details.dart
+++ src/lib/src/pages/details.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import '../libraries/barcode-scanner.dart';
+import '../interfaces/IBarcode.dart';
 
 class DetailsPage extends StatefulWidget {
   final String title;
-  final List<String> codes = new List<String>();
+  final List<Barcode> codes = new List<Barcode>();
   DetailsPage({this.title});
 
   @override
@@ -12,34 +13,21 @@ class DetailsPage extends StatefulWidget {
 
 class _AppDetailsPage extends State<DetailsPage> {
   void onCameraClicked(BuildContext context) {
-    AppBarcodeScanner().startBarcodeScanner().then((v) {
-      print('scanned code is: $v');
+    AppBarcodeScanner().startBarcodeScanner().then((obtainedCode) {
+      var newCode = new Barcode();
+      newCode.code = obtainedCode;
+      newCode.timeStamp = new DateTime.now().toLocal();
       setState(() {
-        widget.codes.add(v);
+        widget.codes.add(newCode);
       });
     });
-  }
-
-  List<ListTile> generateListTiles() {
-    var list = new List<ListTile>();
-    for (var key in widget.codes) {
-      var tile = new ListTile(
-        title: new Text(key),
-        subtitle: new Text(new DateTime.now().toString()),
-      );
-
-      list.add(tile);
-    }
-
-    return list;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
-        leading: Text(widget.codes.length.toString()),
+        title: Text(widget.title + ' (' + widget.codes.length.toString() + ')'),
         actions: <Widget>[
           IconButton(
               icon: Icon(Icons.camera),
@@ -52,7 +40,19 @@ class _AppDetailsPage extends State<DetailsPage> {
             onPressed: () {
               Navigator.pop(context);
             },
-            child: new ListView(children: generateListTiles())),
+            child: new ListView.separated(
+              separatorBuilder: (context, index) => Divider(
+                    color: Colors.red,
+                  ),
+              itemCount: widget.codes.length,
+              itemBuilder: (context, index) => Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: ListTile(
+                      title: Text(widget.codes[index].code),
+                      subtitle: Text(widget.codes[index].timeStamp.toString()),
+                    ),
+                  ),
+            )),
       ),
     );
   }

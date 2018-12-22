27926a0f61739f6aef1585a7006a828e2b82c47a
Sharma, Amit <asharma@agtinternational.com>
2018-12-22 10:55:55 +0100

    WIP: Adding initial version of the credentials page with basic validations.

--- src/lib/src/utils/usernanme-pwd.dart
+++ src/lib/src/utils/usernanme-pwd.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './snackbar.dart';
 
 class UsernamePwdForm extends StatefulWidget {
   @override
@@ -6,12 +7,54 @@ class UsernamePwdForm extends StatefulWidget {
 }
 
 class _UsernamePwdState extends State<UsernamePwdForm> {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return (Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: Text('Credentials'),
       ),
-    ));
+      body: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            TextFormField(
+              decoration: InputDecoration(
+                icon: Icon(Icons.supervised_user_circle),
+                labelText: 'Name *',
+              ),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter username';
+                }
+              },
+            ),
+            TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(
+                icon: Icon(Icons.security),
+                labelText: 'Password *',
+              ),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter the password';
+                }
+              },
+            ),
+            Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: RaisedButton(
+                  onPressed: () {
+                    if (_formKey.currentState.validate()) {
+                      print('Done');
+                    }
+                  },
+                  child: Text('Submit'),
+                ))
+          ],
+        ),
+      ),
+    );
   }
 }

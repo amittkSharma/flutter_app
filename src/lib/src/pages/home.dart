4aee7846cc1ea5f3b7192cd83ba174606fe3ef33
AmittKSharma <amit12.sharma@gmail.com>
2019-01-02 23:22:19 +0100

    Feat: Adding Connectivity package to the app.

--- src/lib/src/pages/home.dart
+++ src/lib/src/pages/home.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:connectivity/connectivity.dart';
 import '../utils/navdrawer.dart';
 import './details.dart';
 import './wifi-connection.dart';
 import './listpage.dart';
 
 class AppHome extends StatelessWidget {
+  String _connectionStatus = 'Unknown';
+  final Connectivity _connectivity = Connectivity();
+
+  AppHome() {}
+
   void addNewItemsToDrawer(AppDrawer drawer) {
     drawer.drawerItems['Barcode Page'] = DetailsPage(title: 'Barcodes');
     drawer.drawerItems['QR Page'] = DetailsPage(title: 'QRs');
     drawer.drawerItems['WiFi Devices'] = WiFiPage();
     drawer.drawerItems['Http Example'] = ListPage();
+  }
+
+  Future<String> initConnectivity() async {
+    String connectionStatus;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      connectionStatus = (await _connectivity.checkConnectivity()).toString();
+    } catch (e) {
+      print(e.toString());
+      connectionStatus = 'Failed to get connectivity.';
+    }
+
+    print('....$connectionStatus');
+    return connectionStatus;
   }
 
   @override
@@ -21,7 +41,16 @@ class AppHome extends StatelessWidget {
       appBar: AppBar(
         title: Text('Home Page'),
       ),
-      body: Center(child: Text('Landing Page...')),
+      body: FutureBuilder<String>(
+        future: initConnectivity(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) print('Error $snapshot.error');
+
+          return snapshot.hasData
+              ? Center(child: Text(snapshot.data))
+              : Center(child: CircularProgressIndicator());
+        },
+      ),
     );
   }
 }
